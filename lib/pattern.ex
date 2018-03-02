61ffe4cfe5410967b887e31203da5e845e285de8
Justin Bean <justin@the.club>
2018-03-02 15:49:26 -0700

    touch readme and format

--- lib/pattern.ex
+++ lib/pattern.ex
@@ -22,20 +22,22 @@ defmodule Waverider.Pattern do
 
   defmacro pattern() do
     quote do
-      <<var!(chunk_id)::binary-size(4),
+      <<
+        var!(chunk_id)::binary-size(4),
         var!(chunk_size)::little-unsigned-integer-size(32),
         var!(format)::binary-size(4),
         var!(sub_chunk_1_id)::binary-size(4),
         var!(sub_chunk_1_size)::little-unsigned-integer-size(32),
         var!(audio_format)::little-unsigned-integer-size(16),
-        var!(num_channels) ::little-unsigned-integer-size(16),
+        var!(num_channels)::little-unsigned-integer-size(16),
         var!(sample_rate)::little-unsigned-integer-size(32),
         var!(byte_rate)::little-unsigned-integer-size(32),
         var!(block_align)::little-unsigned-integer-size(16),
         var!(bits_per_sample)::little-unsigned-integer-size(16),
         var!(sub_chunk_2_id)::binary-size(4),
         var!(sub_chunk_2_size)::little-unsigned-integer-size(32),
-        var!(data)::binary>>
+        var!(data)::binary
+      >>
     end
   end
 end
